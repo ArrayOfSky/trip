6fd9b7441db9801dd243ad0eba94c29f7442f9ed
fire <357578741@qq.com>
2022-09-06 23:20:48 +0800

    1

---
+++ src/main/java/com/cust/trip/bean/Order.java
@@ -0,0 +1,25 @@
+package com.cust.trip.bean;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+/**
+ * @author guoyixing
+ * 2022/9/6
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    private int id;
+    private int proDuctId;
+    private int userId;
+    private Timestamp createTime;
+    private Timestamp updateTime;
+    private int status;
+    private double discount;
+}
