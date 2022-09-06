8940dab8d978a7498dab3f5950507f8308d2d1f1
fire <357578741@qq.com>
2022-09-07 00:13:10 +0800

    1

--- src/main/java/com/cust/trip/dao/OrderMapper.java
+++ src/main/java/com/cust/trip/dao/OrderMapper.java
@@ -1,7 +1,67 @@
 package com.cust.trip.dao;
 
+import com.cust.trip.bean.Order;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+/**
+ * @author guoyixing
+ * 2022/9/6
+ */
 @Mapper
 public interface OrderMapper {
+
+    /**
+     * 获取所有订单信息
+     * @return 订单列表
+     */
+    List<Order> getAllOrders();
+
+
+    /**
+     * 依据商品id获取订单
+     * @param productId 商品id
+     * @return 订单列表
+     */
+    List<Order> getOrderByproductId(int productId);
+
+
+    /**
+     * 依据用户id获取订单
+     * @param userId 用户id
+     * @return 订单列表
+     */
+    List<Order> getOrderByUserId(int userId);
+
+    /**
+     * 依据状态获取订单
+     * @param status 订单状态
+     * @return 订单列表
+     */
+    List<Order> getOrderByStatus(int status);
+
+    /**
+     * 依据日期获取订单
+     * @param createTime 订单生成日期
+     * @return 订单列表
+     */
+    List<Order> getOrderByDate(Timestamp createTime);
+
+    /**
+     * 获取时间段内订单
+     * @param createTimePre 时间（先）
+     * @param createTimePo  时间（后）
+     * @return 订单列表
+     */
+    List<Order> getOrdersBtDates(Timestamp createTimePre,Timestamp createTimePo);
+
+    /**
+     * 获取指定折扣区间内的订单
+     * @param lowDiscount 最低折扣
+     * @param highDiscount 最高折扣
+     * @return 订单列表
+     */
+    List<Order> getOrderBtDisCounts(double lowDiscount,double highDiscount);
 }
--- src/main/java/com/cust/trip/dao/UserMapper.java
+++ src/main/java/com/cust/trip/dao/UserMapper.java
@@ -1,7 +1,100 @@
 package com.cust.trip.dao;
 
+import com.cust.trip.bean.User;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
+/**
+ * @author guoyixing
+ * 2022/9/6
+ */
 @Mapper
 public interface UserMapper {
+    /**
+     * 获取所有用户
+     * @return 用户列表
+     */
+    List<User> getAllUsers();
+
+    /**
+     * 通过用户名获取用户
+     * @param name 用户名
+     * @return 用户列表
+     */
+    List<User> getUsersByName(String name);
+
+    /**
+     * 通过电话号码获取用户
+     * @param phoneNumber 电话号码
+     * @return 用户列表
+     */
+    User       getUserByPhoneNumber(String phoneNumber);
+
+    /**
+     * 依据消费区间获取用户
+     * @param consumption1 低消费
+     * @param consumption2 高消费
+     * @return 用户列表
+     */
+    List<User> getUsersByConsumptions(double consumption1,double consumption2);
+
+    /**
+     * 获取某值以下的消费人群
+     * @param lowConsumption 低值
+     * @return 用户列表
+     */
+    List<User> getUsersByLowConsumption(double lowConsumption);
+
+    /**
+     * 获取某值以上的消费人群
+     * @param highConsumption 高值
+     * @return 用户列表
+     */
+    List<User> getUsersByHighConsumption(double highConsumption);
+
+    /**
+     *获取某区间的消费人群
+     * @param balance1 低余额
+     * @param balance2 高余额
+     * @return 用户列表
+     */
+    List<User> getUsersByBalances(double balance1,double balance2);
+
+    /**
+     * 获取某值以下的消费人群
+     * @param lowBalance 低值
+     * @return 用户列表
+     */
+    List<User> getUsersByLowBalance(double lowBalance);
+
+    /**
+     * 获取某值以上的消费人群
+     * @param highBalance 高值
+     * @return 用户列表
+     */
+    List<User> getUsersByHighBalance(double highBalance);
+
+    /**
+     * 获取某区间的消费人群
+     * @param vip1 低等级
+     * @param vip2 高等级
+     * @return 用户列表
+     */
+    List<User> getUsersByVips(double vip1,double vip2);
+
+    /**
+     * 获取某值以上的消费人群
+     * @param lowVip 低等级
+     * @return 用户列表
+     */
+    List<User> getUsersByLowVip(double lowVip);
+
+    /**
+     * 获取某值以上的消费人群
+     * @param highVip 高等级
+     * @return 用户列表
+     */
+    List<User> getUsersByHighVip(double highVip);
+
 }
