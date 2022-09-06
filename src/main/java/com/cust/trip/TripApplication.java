fd0e7d187ecbd122c54d7f92fb96ed86bd799db8
gaoyifeng <2244952274@qq.com>
2022-09-06 22:02:19 +0800

    test

--- src/main/java/com/cust/trip/TripApplication.java
+++ src/main/java/com/cust/trip/TripApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TripApplication {
-
     public static void main(String[] args) {
-        int a = 1;
         SpringApplication.run(TripApplication.class, args);
     }
-
 }
