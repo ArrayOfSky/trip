39fb4da5287d2b413c19ea69cf1409d2f0194a7c
GYF <2244952274@qq.com>
2022-09-09 12:06:04 +0800

    添加了 KindServiceImpl

--- src/main/java/com/cust/trip/controller/ProductController.java
+++ src/main/java/com/cust/trip/controller/ProductController.java
@@ -1,4 +1,16 @@
 package com.cust.trip.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author GYF
+ * @Data 2022.9.9
+ */
+@RestController
 public class ProductController {
+
+
+
+
+
 }
--- src/main/java/com/cust/trip/service/impl/KindServiceImpl.java
+++ src/main/java/com/cust/trip/service/impl/KindServiceImpl.java
@@ -1,9 +1,12 @@
 package com.cust.trip.service.impl;
 
 import com.cust.trip.bean.Kind;
+import com.cust.trip.dao.KindMapper;
 import com.cust.trip.service.KindService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -13,29 +16,54 @@ import java.util.List;
 @Service
 public class KindServiceImpl implements KindService {
 
+    @Autowired
+    KindMapper kindMapper;
+
     @Override
     public int addKind(Kind kind) {
-        return 0;
+        ArrayList<Kind> array = (ArrayList<Kind>) kindMapper.selectAll();
+        for(Kind a : array){
+            if(a.getName().equals(kind.getName())){
+                return 0;
+            }
+        }
+        kindMapper.insertKind(kind);
+        return 1;
     }
 
     @Override
     public int deleteKind(String name) {
-        return 0;
+        kindMapper.deleteKind(name);
+        return 1;
     }
 
     @Override
     public List<Kind> selectAllKind() {
-        return null;
+        ArrayList<Kind> array = (ArrayList<Kind>) kindMapper.selectAll();
+        return array;
     }
 
     @Override
     public Kind selectKindByName(String name) {
+        ArrayList<Kind> array = (ArrayList<Kind>) kindMapper.selectAll();
+        for(Kind a : array){
+            if(a.getName().equals(name)){
+                return a;
+            }
+        }
         return null;
     }
 
     @Override
     public int updateKind(String name1, String name2) {
-        return 0;
+        ArrayList<Kind> array = (ArrayList<Kind>) kindMapper.selectAll();
+        for (Kind a: array){
+            if(a.getName().equals(name1)){
+                a.setName(name2);//因为后续是通过id来定位kind，我可以直接更改name
+                kindMapper.updateKind(a);
+            }
+        }
+        return 1;
     }
 
 }
