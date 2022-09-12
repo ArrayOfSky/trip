d39e82bedca055c1555609c14591fb8aadf91124
GYF <2244952274@qq.com>
2022-09-13 00:46:55 +0800

    配置 过滤器 设置字符编码

--- src/main/java/com/cust/trip/filter/BaseFilter.java
+++ src/main/java/com/cust/trip/filter/BaseFilter.java
@@ -1,31 +1,42 @@
-//package com.cust.trip.filter;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-///**
-// * @author GYF
-// * @Data 2022.9.12
-// */
-//public class BaseFilter implements Filter {
-//
-//
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//        Filter.super.init(filterConfig);
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletRequest request = (HttpServletRequest) servletRequest;
-//        HttpServletResponse response = (HttpServletResponse) servletResponse;
-//        filterChain.doFilter(request, response);
-//    }
-//
-//    @Override
-//    public void destroy() {
-//        Filter.super.destroy();
-//    }
-//}
+package com.cust.trip.filter;
+
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 设置字符编码
+ * URL级别权限访问控制
+ * 过滤敏感词汇
+ * 压缩响应信息
+ * @author GYF
+ * @Data 2022.9.12
+ */
+@Order(1)
+public class BaseFilter implements Filter {
+
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        Filter.super.init(filterConfig);
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        filterChain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+        Filter.super.destroy();
+    }
+}
