761ecf1019673da8c046fa875ae329fa7cf02028
guoyixing <357578741@qq.com>
2022-09-22 09:11:33 +0800

    配置跨域-gyx

--- src/main/java/com/cust/trip/config/WebMvcConfig.java
+++ src/main/java/com/cust/trip/config/WebMvcConfig.java
@@ -39,20 +39,11 @@ public class WebMvcConfig implements WebMvcConfigurer {
     // 拦截器跨域配置
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // 跨域路径
-        CorsRegistration cors = registry.addMapping("/**");
-
-        // 可访问的外部域
-        cors.allowedOrigins("*");
-        // 支持跨域用户凭证
-        //cors.allowCredentials(true);
-        //cors.allowedOriginPatterns("*");
-        // 设置 header 能携带的信息
-        cors.allowedHeaders("*");
-        // 支持跨域的请求方法
-        cors.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
-        // 设置跨域过期时间，单位为秒
-        cors.maxAge(3600);
+        registry.addMapping("/**")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(1800)
+                .allowedOrigins("*");
     }
 
 }
